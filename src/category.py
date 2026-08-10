cb773bcc5f8dc25cd9f9d2cd76a9791f9176c1ef
svmikhaylov333 <svmikhaylov333@gmail.com>
2026-08-11 00:18:19 +0300

    Доработан метод add_product для Category

--- src/category.py
+++ src/category.py
@@ -34,8 +34,11 @@ class Category:
 
     # метод добавления товаров в категорию
     def add_product(self, product: Product) -> None:
+        if not isinstance(product, Product):
+            raise TypeError(" в список нельзя добавить ничего другого кроме Product и его наследников")
         self.__products.append(product)
         Category.product_count += 1
+
 
     # метод получения списка продуктов в категории
     def get_product_list(self):
